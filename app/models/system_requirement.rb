f97a0f169629a23262e105ce6a8134347e1d00cf
lucasslemos <lucas.lemos.dev@gmail.com>
2022-03-25 10:12:33 -0400

    Model de Jogos

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -5,4 +5,8 @@ class SystemRequirement < ApplicationRecord
   validates :processor, presence: true
   validates :memory, presence: true
   validates :video_board, presence: true
+  
+  has_many :games, dependent: :restrict_with_error
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
